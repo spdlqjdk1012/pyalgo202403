74641922c1238f925e63a7981a6b744562bcb6cd
knote <spdlqjdk1012@naver.com>
2024-03-19 14:08:50 +0900

    20207 0319

--- 20207.py
+++ 20207.py
@@ -1,16 +1,49 @@
 num = int(input())
-mylist = []
+calendar = [0 for i in range(366)]
 for i in range(num):
-    tmp = list(map(int, input().split()))
-    
-    mylist.append(tmp)
-mylist.sort(key=lambda x:x[0])
-#print(mylist)
+    st, end = map(int, input().split())
+    for num in range(st, end+1):
+        calendar[num] = calendar[num]+1
+#print(calendar) 
 
-for tmp in mylist:
-    diff = tmp[1]-tmp[0]+1
-    tmp.append(diff)
-#print(mylist)
+start = 0
+end = 0
+maxval = 0
+width = []
+for i in range(len(calendar)):
+    if calendar[i] != 0:
+        if start == 0:
+            start = i
+        maxval = max(maxval, calendar[i])
 
-mylist.sort(key=lambda x:x[-1], reverse=True)
-print(mylist)
+    if calendar[i] == 0: 
+        if start != 0 and end == 0:
+            end = i
+            w = (end-start) * maxval
+            #print(w)
+            width.append(w)
+            start = 0
+            maxval = 0
+            end = 0
+
+    if i == len(calendar)-1:
+        w = (i-start+1) * maxval
+        width.append(w)
+#print(width)
+print(sum(width))
+
+
+"""
+5
+1 3
+2 4
+5 7
+9 11
+13 13
+18
+
+
+1
+1 365
+# 답 : 365
+"""
