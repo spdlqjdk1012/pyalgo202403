529a8f391568b65b24533514a2883dbb6aa89259
knote <spdlqjdk1012@naver.com>
2024-04-03 10:45:07 +0900

    벨만포드 오류 수정

--- 13.11657.py
+++ 13.11657.py
@@ -1,10 +1,12 @@
 # https://www.acmicpc.net/problem/11657
-INF = int(1e9)
+import sys
+input = sys.stdin.readline
+INF = int(1e10)
 
 V, E = map(int, input().split()) #V노드의수3, E간선의수4
-
 edges = []
 dist = [INF]*(V+1)
+
 for i in range(E):
     st, end, diff = map(int, input().split())
     edges.append((st, end, diff))
@@ -12,13 +14,13 @@ for i in range(E):
 #print(dist)
 #print(edges)
 
-def bellmanFord(st):
-    dist[st] = 0# 1번노드에서 1번노드로 이동하는비용 0
+def bellmanFord(start):
+    dist[start] = 0# 1번노드에서 1번노드로 이동하는비용 0
     for i in range(V):# 0번노드부터 모든 노드 탐색
         for j in range(E): # 모든 간선 확인
             curNode, edgeNode, cost = edges[j]
             #1번노드에서 2번노드로 이동하는 비용4
-            if dist[i] != INF and dist[curNode]+cost<dist[edgeNode]:
+            if dist[curNode] != INF and dist[curNode]+cost<dist[edgeNode]:
                 dist[edgeNode] = dist[curNode]+cost
                 #print(dist)
                 if i == V-1:
@@ -36,3 +38,4 @@ else:
         else:
             print(dist[i])
 # 1번->2번노드로 가는길 갱신 1->2번거쳐서 ->3번노드로 가는길 갱신 ... 
+# https://great-park.tistory.com/134
